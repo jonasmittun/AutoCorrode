f632dc597abc2c3aa60d6486d4dfae4123c3453b
Jónas Mittún Peltonen <71870644+jonasmittun@users.noreply.github.com>
2026-05-06 16:16:39 +0200

    Function definition implementation. WIP contract and proof w/ example

--- Micro_Rust_Std_Lib/StdLib_Ordering.thy
+++ Micro_Rust_Std_Lib/StdLib_Ordering.thy
@@ -26,6 +26,45 @@ definition cmp_pure :: \<open>'a::{order} \<Rightarrow> 'a \<Rightarrow> orderin
 definition cmp :: \<open>'a::{order} \<Rightarrow> 'a \<Rightarrow> ('s, ordering, 'abort, 'i, 'o) function_body\<close> where
   \<open>cmp \<equiv> lift_fun2 cmp_pure\<close>
 
+(*
+ * Function definition for ordering equality
+ *)
+
+definition is_eq_pure :: \<open>ordering  \<Rightarrow> bool\<close> where
+  \<open>is_eq_pure x \<equiv>
+    if x = Equal then True else False\<close>
+
+definition is_eq :: \<open>ordering \<Rightarrow> ('s, bool, 'abort, 'i, 'o) function_body\<close> where
+  \<open>is_eq \<equiv> lift_fun1 is_eq_pure\<close>
+
+(*
+ * Contract definition and proof
+ *)
+
+definition eq_test where \<open>eq_test \<equiv> FunctionBody \<lbrakk>
+    let mut nat_ref_1 = \<llangle>0 :: 64 word\<rrangle>;
+    let mut nat_ref_2 = \<llangle>0 :: 64 word\<rrangle>;
+    let mut bool_ref_result = \<llangle>True :: bool\<rrangle>;
+    let mut temp_order = \<llangle>Equal :: ordering\<rrangle>;
+    temp_order = cmp(*nat_ref_1, *nat_ref_2);
+    bool_ref_result = is_eq(*temp_order);
+    *bool_ref_result
+  \<rbrakk>\<close>
+
+definition eq_test_contract where
+  \<open>eq_test_contract \<equiv>
+     let pre  = can_alloc_reference in
+     let post = \<lambda>r. can_alloc_reference \<star> \<langle>r = True\<rangle> in
+     make_function_contract pre post\<close>
+ucincl_auto eq_test_contract
+
+lemma eq_test_spec:
+  shows \<open>\<Gamma>; eq_test \<Turnstile>\<^sub>F eq_test_contract\<close>
+  apply (crush_boot f: eq_test_def contract: eq_test_contract_def)
+  apply crush_base
+  sorry
+
+
 (*<*)
 end
 (*>*)
